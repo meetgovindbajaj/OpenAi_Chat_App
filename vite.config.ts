904cf256fd896a567987cffa76953c58004efec9
Govind Bajaj <meetgovindbajaj@gmail.com>
2024-01-05 17:44:24 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api/v1": {
+        target: "https://open-ai-chat-app-server.vercel.app",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 });
